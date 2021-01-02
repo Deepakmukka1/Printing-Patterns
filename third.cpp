f36ded13bfd9317ff3494ba1efe0bf149215a1b5
Mukka Deepak <ramu92013@gmail.com>
2021-01-03 00:34:33 +0530

    ⚡️ Added a new Pattern

---
+++ third.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int numberOfRows;
+   cout<<"Enter the number of rows: "<<"\n";
+   cin>>numberOfRows;
+   for(int i=0;i<numberOfRows;i++)
+   {
+   	for(int j=0;j<numberOfRows;j++)
+   	{	
+   	  if(j>=numberOfRows-i-1)
+   		cout<<"*";	
+   		else
+   		cout<<" ";
+	
+	
+	}
+	cout<<"\n";
+   }
+}
+
